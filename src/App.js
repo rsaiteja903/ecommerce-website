8a324de46245c84fa45167d4018e074d19c07648
Sai Teja R <rsaiteja903@gmail.com>
2024-05-09 17:37:29 -0500

    day-two

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigations from './components/navigation/Navigations';
+import HomePage from './components/pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigations/>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainCarosuel/MainCarosuelData.js
@@ -0,0 +1,12 @@
+
+export const MainCarouselData = [
+    {
+        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/_/1_11.jpg"
+    },
+    {
+        image : "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/_/2_16.jpg"
+    },
+    {
+        image : "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/3/_/3_18.jpg"
+    }
+]
